54d0525409919cb823c1ee418bf9075545ca1a46
DocRAID <l01062506145@gmail.com>
2025-06-26 00:31:24 +0900

    Complete message queue introduction and improve file detection threads (after joining the army)

--- src/listener.rs
+++ src/listener.rs
@@ -4,6 +4,7 @@ use std::sync::{Arc, mpsc};
 use std::sync::mpsc::Sender;
 use crate::configure::SourceConfig;
 
+#[derive(Debug)]
 enum ActionDetermine {
     CreateFile,
     CreateDir,
@@ -17,7 +18,13 @@ enum ActionDetermine {
 
     Unknown
 }
-pub fn listner(path: &str, config:Arc<SourceConfig>) -> Result<()> {
+#[derive(Debug)]
+pub struct FileAction {
+    act:ActionDetermine,
+    path:Box<Path>
+}
+
+pub fn listner(action_sender:Sender<FileAction>, path: &str, config:Arc<SourceConfig>) -> Result<()> {
     let (tx, rx) = mpsc::channel::<Result<Event>>();
 
     let mut watcher = notify::recommended_watcher(tx)?;
@@ -37,7 +44,11 @@ pub fn listner(path: &str, config:Arc<SourceConfig>) -> Result<()> {
                 if !(!config.reflect_temporary_file && is_temp_file) {  //임시파일 적용 여부 확인
                     match event.kind {
                         EventKind::Create(_) => {
-
+                            //파일이 만들어진건지, 폴더가 만들어진건지 확인 event.paths.last()
+                            let event_path: Box<Path> = Box::from(
+                                event.paths.last().expect("no path").as_path()
+                            );
+                            action_sender.send(FileAction { act: ActionDetermine::CreateFile, path:event_path }).unwrap()
                         }
                         EventKind::Modify(_) => {
 
--- src/main.rs
+++ src/main.rs
@@ -2,37 +2,57 @@ mod configure;
 mod controler;
 mod listener;
 mod logging;
-mod ipc_module;
 
-use std::sync::Arc;
+use std::path::Path;
+use std::sync::{Arc, mpsc};
+use std::sync::mpsc::RecvError;
 use std::thread;
 use log::info;
 use notify::{Result, Watcher};
 use crate::configure::{get_config};
 use crate::controler::controler;
-use crate::listener::listner;
+use crate::listener::{FileAction, listner};
 use crate::logging::log_init;
 
 fn main() -> Result<()> {
+    let (action_sender, action_receiver) = mpsc::channel(); //이거 네이밍을 다르게 해보자.
+
     let config:configure::Config = match get_config() {
         Ok(conf) => {conf}
         Err(_) => {
             panic!("config error.");
         }
     };
+
     let _log_handle = log_init(r"log/fileDRust.log".parse().unwrap());
     info!("fileDRust agent started.");
 
     // export to process
+    //스레드 핸들 리스트
     let mut handles = Vec::new();
+    //소스 측의 config 불러오기
     let source = Arc::new(config.source.clone());
-    let listen_list = config.source.path_list.clone().unwrap();
-    for dir in listen_list {
+    //source 측 감지 할 경로 리스트
+    let listen_path_list = config.source.path_list.clone().unwrap();
+
+    // path list 만큼 스레드 생성 후 변경 리스너 실행
+    for dir in listen_path_list {
+        let action_sender = action_sender.clone();
         let dir = dir.clone().to_string();
         let source = Arc::clone(&source);
         handles.push(thread::spawn(move || {
-            listner(&*dir, source).unwrap();
+            listner(action_sender,&*dir, source).unwrap();
         }));
+    }
+
+    //test loop
+    loop {
+        match action_receiver.recv() {
+            Ok(x) => {
+                println!("{:?}",x);
+            }
+            Err(_) => {}
+        }
     }
 
     controler();
